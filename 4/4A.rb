062c948d968f9d690ba6f2e3cbb721bc289a90b9
ranilakshmi <aryakunjamma@gmail.com>
2021-04-01 22:20:49 +0530

    ruby

---
+++ 4/4A.rb
@@ -0,0 +1,8 @@
+n = gets.to_i
+if n==2
+    puts "NO"
+elsif n%2==0
+    puts "YES"
+else
+    puts "NO"
+end
---
+++ 617/617A.rb
@@ -0,0 +1,5 @@
+n=gets.to_i
+result=n/5
+if n%5 > 0
+    result = result + 1
+puts result
